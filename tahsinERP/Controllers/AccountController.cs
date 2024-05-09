90a4579d4e37787f81820e3040e22a725defe64d
khojiabror <khojiabror@gmail.com>
2024-05-10 01:21:55 +0500

    user_entry ni har safar yozadigan qildik

--- tahsinERP/Controllers/AccountController.cs
+++ tahsinERP/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +11,7 @@ using System.Web.Script.Serialization;
 using System.Web.Security;
 using tahsinERP.Models;
 using tahsinERP.ViewModels;
+using System.Runtime.InteropServices;
 
 namespace tahsinERP.Controllers
 {
@@ -16,6 +19,11 @@ namespace tahsinERP.Controllers
     {
         string role = "";
         DBTHSNEntities db = new DBTHSNEntities();
+        [DllImport("Iphlpapi.dll")]
+        private static extern int SendARP(int dest, int host, ref long mac, ref int length);
+
+        [DllImport("Ws2_32.dll")]
+        private static extern int inet_addr(string ip);
         public ActionResult Index()
         {
             return View();
@@ -51,24 +59,20 @@ namespace tahsinERP.Controllers
 
                     if (IsValidUser)
                     {
-                        //if (user.KeepMeSigned)
-                        //    FormsAuthentication.SetAuthCookie(user.UName, false);
-                        //else
-                        //{
-                            var authTicket = new FormsAuthenticationTicket(1, user.UName, DateTime.Now, DateTime.Now.AddMinutes(15), false, userData);
-                            string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+                        var authTicket = new FormsAuthenticationTicket(1, user.UName, DateTime.Now, DateTime.Now.AddMinutes(15), false, userData);
+                        string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
 
-                            var cookie = new HttpCookie(FormsAuthentication.FormsCookieName,
-                                encryptedTicket)
-                            {
-                                HttpOnly = true,
-                                Secure = FormsAuthentication.RequireSSL,
-                                Path = FormsAuthentication.FormsCookiePath,
-                                Domain = FormsAuthentication.CookieDomain,
-                                Expires = authTicket.Expiration
-                            };
-                            Response.Cookies.Set(cookie);
-                        //}
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName,
+                            encryptedTicket)
+                        {
+                            HttpOnly = true,
+                            Secure = FormsAuthentication.RequireSSL,
+                            Path = FormsAuthentication.FormsCookiePath,
+                            Domain = FormsAuthentication.CookieDomain,
+                            Expires = authTicket.Expiration
+                        };
+                        Response.Cookies.Set(cookie);
+                        SetUserEntry(getUser.ID);
                         return RedirectToAction("Index", "Home");
                     }
                 }
@@ -76,6 +80,44 @@ namespace tahsinERP.Controllers
             ModelState.AddModelError("", "invalid Username or Password");
             return View();
         }
+
+        private void SetUserEntry(int userID)
+        {
+            try
+            {
+                string ip = string.Empty;
+                IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress[] addr = ipEntry.AddressList;
+                ip = addr[1].ToString();
+
+                int ldest = inet_addr(ip);
+                int lhost = inet_addr("");
+                long macInfo = 0;
+                int len = 6;
+                int result = SendARP(ldest, 0, ref macInfo, ref len);
+                string macSrc = macInfo.ToString("X");
+                while (macSrc.Length < 12)
+                {
+                    macSrc = macSrc.Insert(0, "0");
+                }
+                string macAddress = string.Join(":", Enumerable.Range(0, 12).Where(x => x % 2 == 0).Select(x => macSrc.Substring(x, 2)));
+                var userEntry = new USER_ENTRIES
+                {
+                    UserID = userID,
+                    DateTime = DateTime.Now,
+                    IP = ip,
+                    MAC = macAddress
+                };
+
+                db.USER_ENTRIES.Add(userEntry);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+        }
+
         public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
