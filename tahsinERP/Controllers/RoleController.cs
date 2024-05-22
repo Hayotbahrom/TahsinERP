de169ebe9ed94eee6151845c3c833bfbce0bf967
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-05-22 08:29:54 +0500

    RoleController yozildi

--- tahsinERP/Controllers/RoleController.cs
+++ tahsinERP/Controllers/RoleController.cs
@@ -1,15 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using tahsinERP.Models;
 
 namespace tahsinERP.Controllers
 {
     public class RoleController : Controller
     {
         // GET: Role
+        DBTHSNEntities db = new DBTHSNEntities();
         public ActionResult Index()
+        {
+            var roles = db.ROLES.Where(r => r.IsDeleted == false).ToList();
+            return View(roles);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ROLES role)
+        {
+            try
+            {
+                ROLES roles = new ROLES();
+                roles.RName = role.RName;
+                roles.Description = role.Description;
+                roles.PERMISSIONS = role.PERMISSIONS;
+                role.IsDeleted = false;
+
+                db.ROLES.Add(roles);
+                db.SaveChanges();
+
+                return View(roles);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(ex.Message, "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int? ID)
+        {
+            if (ID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ROLES role = new ROLES();
+
+            var editrole = db.ROLES.Find(ID);
+            if (editrole == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                role.RName = editrole.RName;
+                role.Description = editrole.Description;
+            }
+
+            return View(role);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int? ID, FormCollection collection)
         {
             return View();
         }
