3e1cb0532682fd2bb9a55f98d52ac3af173bf98a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-05-24 08:45:44 +0500

    save

---
+++ tahsinERP/Models/RoleGiver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace tahsinERP.Models
+{
+    public static class RoleGiver
+    {
+        public static string[] GetUserRoles(string username)
+        {
+            if (string.IsNullOrEmpty(username)) return null;
+            return Roles.GetRolesForUser(username);
+        }
+    }
+}
