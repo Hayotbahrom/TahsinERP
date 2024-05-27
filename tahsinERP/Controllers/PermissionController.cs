4cb76f62ff6f3956d635a0523cf8e360ab2e5e41
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-05-27 16:47:38 +0500

    PermissionModulega Create, Update, Delete Actionlar yozildi

---
+++ tahsinERP/Controllers/PermissionController.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using tahsinERP.Models;
+
+namespace tahsinERP.Controllers
+{
+    public class PermissionController : Controller
+    {
+        DBTHSNEntities db = new DBTHSNEntities();
+        // GET: Permission
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Permissions(int? roleID)
+        {
+            var thisRole = db.ROLES.Find(roleID);
+
+            return View(thisRole);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Permissions(int? roleID, FormCollection fvm)
+        {
+            var permissions = db.PERMISSIONS.Where(pr => pr.RoleID == roleID).ToList();
+            foreach (var permission in permissions)
+            {
+                bool changePermit = permission.ChangePermit;
+                bool viewPermit = permission.ViewPermit;
+
+                // Update the database with the new permission values
+                var perm = db.PERMISSIONS.Find(permission.ID);
+                if (perm != null)
+                {
+                    perm.ChangePermit = changePermit;
+                    perm.ViewPermit = viewPermit;
+                    db.Entry(perm).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+
+            // Redirect to a relevant page after saving the changes
+            return RedirectToAction("Index", "Role");
+        }
+
+        public class PermissionViewModel
+        {
+            public int PermissionId { get; set; }
+            public bool ChangePermit { get; set; }
+            public bool ViewPermit { get; set; }
+        }
+    }
+}
--- tahsinERP/Controllers/RoleController.cs
+++ tahsinERP/Controllers/RoleController.cs
@@ -133,43 +133,43 @@ namespace tahsinERP.Controllers
             roles.Description = role.Description;
             return View(roles);
         }
-        public ActionResult Permissions(int? roleID)
-        {
-            var thisRole = db.ROLES.Find(roleID);
+        //public ActionResult Permissions(int? roleID)
+        //{
+        //    var thisRole = db.ROLES.Find(roleID);
 
-            return View(thisRole);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Permissions(int? roleID, FormCollection fvm)
-        {
-            var permissions = db.PERMISSIONS.Where(pr => pr.RoleID == roleID).ToList();
-            foreach (var permission in permissions)
-            {
-                bool changePermit = permission.ChangePermit;
-                bool viewPermit = permission.ViewPermit;
+        //    return View(thisRole);
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Permissions(int? roleID, FormCollection fvm)
+        //{
+        //    var permissions = db.PERMISSIONS.Where(pr => pr.RoleID == roleID).ToList();
+        //    foreach (var permission in permissions)
+        //    {
+        //        bool changePermit = permission.ChangePermit;
+        //        bool viewPermit = permission.ViewPermit;
 
-                // Update the database with the new permission values
-                var perm = db.PERMISSIONS.Find(permission.ID);
-                if (perm != null)
-                {
-                    perm.ChangePermit = changePermit;
-                    perm.ViewPermit = viewPermit;
-                    db.Entry(perm).State = System.Data.Entity.EntityState.Modified;
-                    db.SaveChanges();
-                }
-            }
+        //        // Update the database with the new permission values
+        //        var perm = db.PERMISSIONS.Find(permission.ID);
+        //        if (perm != null)
+        //        {
+        //            perm.ChangePermit = changePermit;
+        //            perm.ViewPermit = viewPermit;
+        //            db.Entry(perm).State = System.Data.Entity.EntityState.Modified;
+        //            db.SaveChanges();
+        //        }
+        //    }
 
-            // Redirect to a relevant page after saving the changes
-            return RedirectToAction("Index");
-        }
+        //    // Redirect to a relevant page after saving the changes
+        //    return RedirectToAction("Index");
+        //}
 
-        public class PermissionViewModel
-        {
-            public int PermissionId { get; set; }
-            public bool ChangePermit { get; set; }
-            public bool ViewPermit { get; set; }
-        }
+        //public class PermissionViewModel
+        //{
+        //    public int PermissionId { get; set; }
+        //    public bool ChangePermit { get; set; }
+        //    public bool ViewPermit { get; set; }
+        //}
     }
 
 }
