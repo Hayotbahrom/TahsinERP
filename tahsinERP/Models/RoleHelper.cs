8118d9f472e1b959425c5fe4f2c2f90f83bfabb6
khojiabror <khojiabror@gmail.com>
2024-05-28 10:43:12 +0500

    role ni permissioni borligini tekshirish logikasi rolehelper ga qo'shildi va layout page da implement qilindi

--- tahsinERP/Models/RoleHelper.cs
+++ tahsinERP/Models/RoleHelper.cs
@@ -1,13 +1,56 @@
-﻿using System.Web.Security;
+﻿using System.Linq;
+using System.Web.Security;
 
 namespace tahsinERP.Models
 {
     public static class RoleHelper
     {
+        private static DBTHSNEntities db = new DBTHSNEntities();
         public static string[] GetUserRoles(string username)
         {
             if (string.IsNullOrEmpty(username)) return null;
             return Roles.GetRolesForUser(username);
+        }
+        public static bool IsViewPermitted(string username, string moduleName)
+        {
+            string roleName = GetUserRoles(username)[0];
+            ROLES role = db.ROLES.Where(r => r.RName.CompareTo(roleName) == 0).FirstOrDefault();
+            if (role != null)
+            {
+                PERMISSIONS permit = GetPermissionsOfRole(role, moduleName);
+                if (permit != null)
+                {
+                    if (permit.ViewPermit)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            return false;
+        }
+        public static bool IsChangePermitted(string username, string moduleName)
+        {
+            string roleName = GetUserRoles(username)[0];
+            ROLES role = db.ROLES.Where(r => r.RName.CompareTo(roleName) == 0).FirstOrDefault();
+            if (role != null)
+            {
+                PERMISSIONS permit = GetPermissionsOfRole(role, moduleName);
+                if (permit != null)
+                {
+                    if (permit.ChangePermit)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            return false;
+        }
+        private static PERMISSIONS GetPermissionsOfRole(ROLES role, string moduleName)
+        {
+            PERMISSIONMODULE module = db.PERMISSIONMODULES.Where(pm => pm.Module.CompareTo(moduleName) == 0).FirstOrDefault();
+            if (module != null)
+                return db.PERMISSIONS.Where(p => p.RoleID == role.ID && p.PermissionModuleID == module.ID).FirstOrDefault();
+            return null;
         }
     }
 }
