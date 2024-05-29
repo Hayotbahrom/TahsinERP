88b2916a855d17e078b8582288ae6c308a3d44bc
khojiabror <khojiabror@gmail.com>
2024-05-29 16:01:59 +0500

    notfound implement qilindi

--- tahsinERP/Global.asax.cs
+++ tahsinERP/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,13 @@ namespace tahsinERP
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            // Здесь можно выполнить логирование ошибки.
+            // Далее перенаправьте на страницу 404.
+            Response.Redirect("~/Error/NotFound");
         }
     }
 }
