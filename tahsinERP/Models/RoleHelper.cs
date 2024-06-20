2b6719371830b1e34f48912c2b4b0bbaf1f02473
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2024-06-20 23:53:44 +0500

    IsChangePermit, IsViewPermit override => RoleHelper

--- tahsinERP/Models/RoleHelper.cs
+++ tahsinERP/Models/RoleHelper.cs
@@ -61,6 +61,30 @@ namespace tahsinERP.Models
                 return false;
             }
         }
+        public static bool IsViewPermitted(string username, string controller, string action, string parametr)
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                string roleName = GetUserRoles(username)[0];
+
+                // Perform CPU-bound work here
+                // For example, heavy computations or other synchronous tasks
+                ROLE role = db.ROLES.Where(r => r.RName.CompareTo(roleName) == 0).FirstOrDefault();
+                if (role != null)
+                {
+                    PERMISSION permit = GetPermissionsOfRole(role, controller, action, parametr);
+                    if (permit != null)
+                    {
+                        if (permit.ViewPermit)
+                            return true;
+                        else
+                            return false;
+                    }
+                }
+                return false;
+            }
+        }
+
         public static bool IsChangePermitted(string username, string moduleName)
         {
             using (DBTHSNEntities db = new DBTHSNEntities())
@@ -101,6 +125,26 @@ namespace tahsinERP.Models
                 return false;
             }
         }
+        public static bool IsChangePermitted(string username, string controller, string action, string parametr)
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                string roleName = GetUserRoles(username)[0];
+                ROLE role = db.ROLES.Where(r => r.RName.CompareTo(roleName) == 0).FirstOrDefault();
+                if (role != null)
+                {
+                    PERMISSION permit = GetPermissionsOfRole(role, controller, action, parametr);
+                    if (permit != null)
+                    {
+                        if (permit.ChangePermit)
+                            return true;
+                        else
+                            return false;
+                    }
+                }
+                return false;
+            }
+        }
         public static PERMISSION GetPermissionsOfRole(ROLE role, string moduleName)
         {
             using (DBTHSNEntities db = new DBTHSNEntities())
@@ -116,6 +160,16 @@ namespace tahsinERP.Models
             using (DBTHSNEntities db = new DBTHSNEntities())
             {
                 PERMISSIONMODULE module = db.PERMISSIONMODULES.Where(pm => pm.Controller.CompareTo(controller) == 0 && pm.Action.CompareTo(action) == 0).FirstOrDefault();
+                if (module != null)
+                    return db.PERMISSIONS.Where(p => p.RoleID == role.ID && p.PermissionModuleID == module.ID).FirstOrDefault();
+                return null;
+            }
+        }
+        public static PERMISSION GetPermissionsOfRole(ROLE role, string controller, string action, string parametr)
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                PERMISSIONMODULE module = db.PERMISSIONMODULES.Where(pm => pm.Controller.CompareTo(controller) == 0 && pm.Action.CompareTo(action) == 0 && pm.Parameter.CompareTo(parametr) == 0).FirstOrDefault();
                 if (module != null)
                     return db.PERMISSIONS.Where(p => p.RoleID == role.ID && p.PermissionModuleID == module.ID).FirstOrDefault();
                 return null;
