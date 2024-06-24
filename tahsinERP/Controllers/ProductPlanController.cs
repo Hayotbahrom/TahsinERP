c1b592a3ae9d4bd2e8061a60e7e6283eac01ef79
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2024-06-24 13:14:42 +0500

    ProductPlan CRUD boldi

--- tahsinERP/Controllers/ProductPlanController.cs
+++ tahsinERP/Controllers/ProductPlanController.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using tahsinERP.Models;
 
 namespace tahsinERP.Controllers
 {
@@ -10,6 +11,130 @@ namespace tahsinERP.Controllers
     {
         // GET: ProductPlan
         public ActionResult Index()
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+
+                var productplan = db.PRODUCTPLANS.Where(x => x.IsDeleted == false).ToList();
+                return View(productplan);
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                PRODUCTPLAN productPlan = db.PRODUCTPLANS
+                                           .Include(pp => pp.PRODUCT) 
+                                           .FirstOrDefault(pp => pp.ID == id);
+
+                if (productPlan == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(productPlan);
+
+            }
+        }
+
+        public ActionResult Create()
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                ViewBag.ProductList = new SelectList(db.PRODUCTS.Where(p => p.IsDeleted == false).ToList(), "ID", "Name");
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ProductID, PlannedQty, Label, IssueDate, DueDate, IsDeleted")] PRODUCTPLAN productPlan)
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        productPlan.IsDeleted = false;
+                        db.PRODUCTPLANS.Add(productPlan);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Error: " + ex.Message);
+                }
+                ViewBag.ProductList = new SelectList(db.PRODUCTS.Where(p => p.IsDeleted == false).ToList(), "ID", "Name", productPlan.ProductID);
+            }
+            return View(productPlan);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                PRODUCTPLAN productPlan = db.PRODUCTPLANS.Find(id);
+                if (productPlan == null)
+                {
+                    return HttpNotFound();
+                }
+
+                ViewBag.ProductList = new SelectList(db.PRODUCTS.Where(p => p.IsDeleted == false).ToList(), "ID", "Name", productPlan.ProductID);
+                return View(productPlan);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "ID,ProductID,PlannedQty,Label,IssueDate,DueDate,IsDeleted")] PRODUCTPLAN productPlan)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (DBTHSNEntities db = new DBTHSNEntities())
+                    {
+                        var existingProductPlan = db.PRODUCTPLANS.Find(productPlan.ID);
+                        if (existingProductPlan != null)
+                        {
+                            existingProductPlan.ProductID = productPlan.ProductID;
+                            existingProductPlan.PlannedQty = productPlan.PlannedQty;
+                            existingProductPlan.Label = productPlan.Label;
+                            existingProductPlan.IssueDate = productPlan.IssueDate;
+                            existingProductPlan.DueDate = productPlan.DueDate;
+                            existingProductPlan.IsDeleted = false;
+
+                            db.Entry(existingProductPlan).State = EntityState.Modified;
+                            db.SaveChanges();
+                        }
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Tasdiqlanmadi: " + ex.Message);
+                }
+            }
+
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                ViewBag.ProductList = new SelectList(db.PRODUCTS.Where(p => p.IsDeleted == false).ToList(), "ID", "Name", productPlan.ProductID);
+            }
+
+            return View(productPlan);
+        }
+
+        public ActionResult Delete()
         {
             return View();
         }
