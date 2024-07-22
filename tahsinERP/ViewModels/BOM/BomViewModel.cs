1ee3ca96be38b4ff5228a1b824a6bb93880aed30
Oybek <muxtaraliyevoybek@gmail.com>
2024-07-22 18:52:17 +0500

    bomViewmodel changed

--- tahsinERP/ViewModels/BOM/BomViewModel.cs
+++ tahsinERP/ViewModels/BOM/BomViewModel.cs
@@ -16,21 +16,5 @@ namespace tahsinERP.ViewModels.BOM
         public string Process { get; set; }
         public int[] SelectedProcessIds { get; set; }
         public bool IsActive { get; set; }
-
-        public List<BomPart> BomList { get; set; }
-
-        public BomViewModel()
-        {
-            BomList = new List<BomPart>();
-        }
-    }
-
-    public class BomPart
-    {
-        public int PartID { get; set; }
-        public PART PART { get; set; }
-        public int Quantity { get; set; }
-        public string Unit { get; set; }
-        public bool InHouse { get; set; }
     }
 }
