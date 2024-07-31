abb26275ad6a472fc62f133413b0ee8c96a32e71
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2024-07-31 12:22:51 +0500

    Stock() view

--- tahsinERP/Controllers/PartStockController.cs
+++ tahsinERP/Controllers/PartStockController.cs
@@ -16,8 +16,25 @@ namespace tahsinERP.Controllers
         {
             using (DBTHSNEntities db = new DBTHSNEntities())
             {
-                var list = await db.PART_STOCKS.ToListAsync();
+                var userID = GetUserID(User.Identity.Name);
+                var list = await db.PART_STOCKS
+                                        .Include(x => x.PART_WRHS)
+                                        .Include(x => x.PART)
+                                        .Where(x => x.PART_WRHS.MRP == userID)            
+                                        .ToListAsync();
                 return View(list);
+            }
+        }
+
+        private int? GetUserID(string email)
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                USER currentUser = db.USERS
+                                     .Where(u => u.Email.CompareTo(email) == 0 && u.IsDeleted == false && u.IsActive == true)
+                                     .FirstOrDefault();
+
+                return currentUser?.ID;
             }
         }
     }
