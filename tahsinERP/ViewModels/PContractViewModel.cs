ef632693a98f17394d5e5244f11f4cef72559429
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2024-07-31 12:08:50 +0500

    Error profing: PContract

--- tahsinERP/ViewModels/PContractViewModel.cs
+++ tahsinERP/ViewModels/PContractViewModel.cs
@@ -8,7 +8,6 @@ namespace tahsinERP.ViewModels
 {
     public class PContractViewModel
     {
-        public int ID { get; set; }
         public string ContractNo { get; set; }
         [DataType(DataType.Upload)]
         public HttpPostedFileBase File;
@@ -29,13 +28,13 @@ namespace tahsinERP.ViewModels
     // Part ViewModel
     public class PContractPart
     {
-        public int ID { set; get; } // Auto Increment in DB
-        public int ContractID { get; set; } // User dan olinmaydi
+        //public int ID { set; get; } // Auto Increment in DB
+       // public int ContractID { get; set; } // User dan olinmaydi
         public int PartID { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
         public int UnitID { get; set; }
-        public float Amount { get; set; } // Trigger in DB
+       // public float Amount { get; set; } // Trigger in DB
         public float MOQ { get; set; } 
         public bool ActivePart { get; set; }
     }
