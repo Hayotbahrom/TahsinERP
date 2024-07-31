5376ec60556e6c9bc9b21b94a8ad8448449bd55d
Oybek <muxtaraliyevoybek@gmail.com>
2024-08-01 01:40:22 +0500

    birlashtirish

--- tahsinERP/Controllers/SOrderController.cs
+++ tahsinERP/Controllers/SOrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tahsinERP.Models;
 
 namespace tahsinERP.Controllers
 {
@@ -11,6 +12,20 @@ namespace tahsinERP.Controllers
         // GET: SOrder
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                ViewBag.Customers = new SelectList(db.CUSTOMERS.Where(x => x.IsDeleted == false).ToList(), "ID", "Name");
+                ViewBag.SContract = new SelectList(db.S_CONTRACTS.Where(x => x.IsDeleted == false).ToList(), "ID", "ContractNo");
+                ViewBag.Units = new SelectList(db.UNITS.ToList(), "ID", "ShortName");
+                ViewBag.ProductList = new SelectList(db.PRODUCTS.Where(x => x.IsDeleted == false).ToList(), "ID", "PNo");
+            }
+
             return View();
         }
     }
