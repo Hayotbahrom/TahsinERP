3c13ff151a211588e929262094c1b9699d07f215
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2024-07-31 12:09:51 +0500

    Stock View()

---
+++ tahsinERP/Controllers/PartStockController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using tahsinERP.Models;
+
+namespace tahsinERP.Controllers
+{
+    public class PartStockController : Controller
+    {
+        // GET: PartStock
+        public async Task<ActionResult> Index()
+        {
+            using (DBTHSNEntities db = new DBTHSNEntities())
+            {
+                var list = await db.PART_STOCKS.ToListAsync();
+                return View(list);
+            }
+        }
+    }
+}
