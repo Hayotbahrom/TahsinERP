c2348e3f96ba473374c827ec556afe7311ea52c1
Oybek-M <muxtaraliyevoybek@gmail.com>
2024-09-07 21:45:54 +0500

    Controller and View -> UserManual

--- tahsinERP/Controllers/ManualController.cs
+++ tahsinERP/Controllers/ManualController.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Web.Mvc;
+
+namespace tahsinERP.Controllers
+{
+    public class ManualController : Controller
+    {
+        // GET: Manual
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
