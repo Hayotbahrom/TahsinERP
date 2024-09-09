246d99d09adb9125e2da8b527de18a9ef5f47464
Oybek-M <muxtaraliyevoybek@gmail.com>
2024-09-09 17:22:51 +0500

    AdminPanel.LastAction added Filters: Select User(by DropDownList), Sort between two Dates.

--- tahsinERP/Controllers/LastActionsController.cs
+++ tahsinERP/Controllers/LastActionsController.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using tahsinERP.Models;
 
@@ -8,11 +11,30 @@ namespace tahsinERP.Controllers
     public class LastActionsController : Controller
     {
         // GET: LastActions
-        public ActionResult Index()
+        public async Task<ActionResult> Index(int userId = 1,
+                                              DateTime? startDate = null,
+                                              DateTime? endDate = null)
         {
-            using(DBTHSNEntities db = new DBTHSNEntities())
+            // Agar startDate yoki endDate null bo'lsa, default qiymatlarni beramiz
+            startDate = startDate ?? DateTime.Now.AddDays(-30); // So'nggi 30 kunlik ma'lumot
+            endDate = endDate ?? DateTime.Now; // Bugungi sana
+
+            using (DBTHSNEntities db = new DBTHSNEntities())
             {
-                List<USERLOG> logs = db.USERLOGS.ToList();
+                // User loglarini berilgan userId va sanalar oralig'ida filterlash
+                List<USERLOG> logs = await db.USERLOGS
+                    .Where(x => x.UserID == userId
+                                && x.DateTime >= startDate
+                                && x.DateTime <= endDate)
+                    .ToListAsync();
+
+                // Userlarni DropDownList uchun tayyorlash
+                ViewBag.Users = new SelectList(await db.USERS.ToListAsync(), "ID", "Uname", userId);
+
+                // Foydalanuvchiga form orqali jo'natiladigan start va end sanalar
+                ViewBag.StartDate = startDate.Value.ToString("dd-MM-yyyy");
+                ViewBag.EndDate = endDate.Value.ToString("dd-MM-yyyy");
+
                 return View(logs);
             }
         }
